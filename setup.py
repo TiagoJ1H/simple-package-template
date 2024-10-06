9a90e45af3ab7c56e9c3c6e18067b5b5af9d30e3
TiagoJ1H <tiagojh08@gmail.com>
2024-10-06 00:07:03 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="projeto_teste",
     version="0.0.1",
     author="TiagoJ1H",
     author_email="tiagojh08@gmail.com",
